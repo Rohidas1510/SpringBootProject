5f5d7dbaf959f3608dcccf0e7c86af1ff7e66273
ME <ME@DESKTOP-UV0QA6T>
2022-10-20 13:38:56 +0530

    Assignment3

--- Rohidas/src/rohidas/Assignment2.java
+++ Rohidas/src/rohidas/Assignment2.java
@@ -1,10 +1,28 @@
 package rohidas;
-
+import java.util.Scanner;
 public class Assignment2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				  
+					Scanner in = new Scanner(System.in);
+					   System.out.print("Enter a number: ");
+					    int n=in.nextInt();
+					 
+					    for(int i=1;i<=n;i++) 
+					    {
+					      for(int j=i;j<=n-1;j++)
+					      {
+					        System.out.print("");
+					      }
+					      for(int j=1;j<=i;j++) {
+					        System.out.print(j);
+					      }
+					      for(int j=i-1;j>=1;j--) {
+					        System.out.print(j);
+					      }
+					      System.out.println();
+					    }
+					  }
 
 }
