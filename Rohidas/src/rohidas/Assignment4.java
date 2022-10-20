58fdbe31334cb071749b107a8ea3bcf75328fdf7
ME <ME@DESKTOP-UV0QA6T>
2022-10-20 13:55:01 +0530

    HCF

--- Rohidas/src/rohidas/Assignment4.java
+++ Rohidas/src/rohidas/Assignment4.java
@@ -2,9 +2,35 @@ package rohidas;
 
 public class Assignment4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
 
-	}
+						        int row = 2, column = 3;
+						        int[][] matrix = { {1,2}, {4,5},{8,9}};
+
+						        // Display current matrix
+						        display(matrix);
+
+						        // Transpose the matrix
+						        int[][] transpose = new int[column][row];
+						        for(int i = 0; i < row; i++) {
+						            for (int j = 0; j < column; j++) {
+						                transpose[j][i] = matrix[i][j];
+						            }
+						        }
+
+						        // Display transposed matrix
+						        display(transpose);
+						    }
+
+						    public static void display(int[][] matrix) {
+						        System.out.println("The matrix is: ");
+						        for(int[] row : matrix) {
+						            for (int column : row) {
+						                System.out.print(column + "    ");
+						            }
+						            System.out.println();
+						        }
+						    }
 
 }
