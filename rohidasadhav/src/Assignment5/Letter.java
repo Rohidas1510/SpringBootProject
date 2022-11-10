8f4a9eca13d774ff494bfa9767fd74ec08153ff2
ME <ME@DESKTOP-UV0QA6T>
2022-11-10 12:46:26 +0530

    Letter

---
+++ rohidasadhav/src/Assignment5/Letter.java
@@ -0,0 +1,11 @@
+package Assignment5;
+import java.lang.*;
+public class Letter{  
+public static void main(String args[]){  
+String s1="HeLlo";  
+String s1lower=s1.toLowerCase(); 
+String s1upper=s1.toUpperCase();
+System.out.println(s1lower);
+System.out.println(s1upper);
+}}  
+
