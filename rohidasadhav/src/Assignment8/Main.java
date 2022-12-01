bc94b25ba82451ce83fda507d5d16cf193615415
ME <ME@DESKTOP-UV0QA6T>
2022-12-01 12:22:05 +0530

    1/12/2022

---
+++ rohidasadhav/src/Assignment8/Main.java
@@ -0,0 +1,26 @@
+package Assignment8;
+class Circle
+{
+	static final double pi = Math.PI;
+	
+
+	public void Area(double r,double r1)
+	{
+		double area=pi*r*r;
+		System.out.println("First Area of circle="+area);
+		
+		double area1=pi*r1*r1;
+		System.out.println("Second Area of circle="+area1);
+	}
+}
+public class Main
+{ 
+    public static void main(String[] args)
+    {
+		
+		Circle A=new Circle();
+		A.Area(4,20);
+		}
+
+	}
+
