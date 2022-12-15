52bee3378be83b4b11db1f87532b3c8450a62d12
ME <ME@DESKTOP-UV0QA6T>
2022-12-15 14:01:58 +0530

    15/12/2022

---
+++ rohidasadhav/src/Assignment10/Address.java
@@ -0,0 +1,10 @@
+package Assignment10;
+
+public class Address {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ rohidasadhav/src/Assignment10/Apps.java
@@ -0,0 +1,10 @@
+package Assignment10;
+
+public class Apps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
